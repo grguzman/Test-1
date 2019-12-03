50fffeaac2fb498631324a31309a8dd4f69f6939
grguzman <55497579+grguzman@users.noreply.github.com>
2019-12-03 10:53:59 -0500

    Add files via upload

---
+++ test.js
@@ -0,0 +1,22 @@
+const readlineSync = require("readline-sync");
+
+let c= circle
+let r= rectangle
+let t= triangle
+let s=square
+
+let input = i
+
+If i = c
+c= (π*r*2)
+
+If i = s
+s= l × w
+
+If i = r
+r= s × s
+
+If i = t
+t= b h 2
+
+console.log("\nEnter a shape:")
